c109abd2e73f16d7fe5e78ebcd2c06411e1303c0
Mahesh Yadav <mahesh27dx@gmail.com>
2023-02-28 16:29:23 +0100

    characterize the contents clearly

---
+++ job.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+timestart=$(date +"%s")
+
+python3 simulation_run.py -dt="0.001" -time="100000" -temp="300" -period="100"
+
+
+timeend=$(date +"%s")
+
+iduration=$(awk "BEGIN {print ($timeend - $timestart) / 3600}")
+
+echo "Simulation took $duration hours to complete"
+
+#### Following commands can be used #####
+
+# chmod ugo+x a.out
+#mv a.out ~/binf77/ising_2d_simulation
+
+# chmod ugo+x a.out
+#mv a.out ~/binf77/ising_2d_simulation
